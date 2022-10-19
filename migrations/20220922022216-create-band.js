c20760ab9d437c1c22cabab467b3649831c5e420
cre8ivesolutions <tammerz78@gmail.com>
2022-10-19 17:38:56 -0500

    assignment done

--- migrations/20220922022216-create-band.js
+++ migrations/20220922022216-create-band.js
@@ -22,14 +22,6 @@ module.exports = {
       },
       end_time: {
         type: Sequelize.DATE
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
